655620022b0c5bd1ec3e0066bd62ca7ad8666445
Lakym972 <132657810+Lakym972@users.noreply.github.com>
2024-01-22 16:12:05 +0100

    ajout message d'erreur inscription

--- src/controllers/RegisterController.js
+++ src/controllers/RegisterController.js
@@ -17,6 +17,14 @@ class RegisterController {
         newUser.save().then(() => {
             req.flash('notify', 'Votre compte a bien été crée !');
             res.redirect('/');
+        }).catch((error) => {
+            if(error.code == 11000 && error.keyPattern.email) {
+                res.render('register/index', {user : newUser, error : `L'email ${error.keyValue.email} est déjà utilisé !`});
+            } else if(error.code == 11000 && error.keyPattern.username) {
+                res.render('register/index', {user : newUser, error : `Le nom d'utilisateur ${error.keyValue.username} est déjà utilisé !`});
+            } else {
+                res.render('register/index', {user : newUser, error : 'Une erreur est survenue !'});
+            }
         })
     }
 }
