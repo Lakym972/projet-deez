54bd3ac107ee580be3b46686151cdc81212195ab
Lakym972 <132657810+Lakym972@users.noreply.github.com>
2024-01-23 12:14:23 +0100

    ajout login

--- src/controllers/LoginController.js
+++ src/controllers/LoginController.js
@@ -6,35 +6,39 @@ import Cookies from 'cookies';
 class LoginController {
 
     index(req, res) {
-        res.render('login/index', {user : {}});
+        res.render('login/index', { user: {} });
     }
 
-    login(req, res) {
+    async login(req, res) {
         let currentUser = null;
-        currentUser = UserRepository.findOne({email: req.body.email, password: req.body.password});
+        currentUser = await UserRepository.findOne({ $or: [{ email: req.body.email}, {username: req.body.email}] });
 
-        if(currentUser) {
-            if(currentUser.enable_a2f && currentUser.enable_a2f == true) {
-                req.session.user = {
-                    username: currentUser.username,
-                    email: currentUser.email,
-                    roles: currentUser.roles,
-                    conected: false
+        if (currentUser) {
+            if (bcrypt.compareSync(req.body.password, currentUser.password)) {
+                if (currentUser.enable_a2f && currentUser.enable_a2f == true) {
+                    req.session.user = {
+                        username: currentUser.username,
+                        email: currentUser.email,
+                        roles: currentUser.roles,
+                        conected: false
+                    }
+                    res.redirect('/a2f');
+                } else {
+                    req.session.user = {
+                        username: currentUser.username,
+                        email: currentUser.email,
+                        roles: currentUser.roles,
+                        conected: true
+                    };
+                    const token = jwt.sign({ email: currentUser.email }, process.env.JWT_SECRET);
+                    const cookies = new Cookies(req, res);
+                    cookies.set('jwt', token, { httpOnly: true });
+
+                    req.flash('notify', 'Connexion reussie !');
+                    res.redirect('/');
                 }
-                res.redirect('/a2f');
             } else {
-                req.session.user = {
-                    username: currentUser.username,
-                    email: currentUser.email,
-                    roles: currentUser.roles,
-                    conected: true
-                };
-                const token = jwt.sign({email: currentUser.email}, process.env.JWT_SECRET);
-                const cookies = new Cookies(req, res);
-                cookies.set('jwt', token, { httpOnly: true});
-
-                req.flash('notify', 'Connexion reussie !');
-                res.redirect('/');
+                res.render('login/index', { error: `Identifiants incorrects`,user: {email: req.body.email} });
             }
         }
     }
@@ -42,7 +46,7 @@ class LoginController {
     logout(req, res) {
         req.session.user = null;
         const cookies = new Cookies(req, res);
-        cookies.set('jwt', '', {expires: 0});
+        cookies.set('jwt', '', { expires: 0 });
         req.flash('notify', 'Deconnexion reussie !');
         res.redirect('/');
     }
