1cf78781ac059fa5912f0ba84ad4d91ec8817349
Lakym972 <132657810+Lakym972@users.noreply.github.com>
2024-01-26 15:31:18 +0100

    modif editPlaylist permet d'ajouter des sons aux playlist

--- src/controllers/AdminController.js
+++ src/controllers/AdminController.js
@@ -34,15 +34,31 @@ class AdminController {
     }
 
     editPlaylist(req, res) {
-        PlayListRepository.findOne({ _id: req.params.id }).then(playlist => {
-            res.render('admin/playlist_edit', { playlist })
+        musicRepository.find({}).then(musics => {
+            PlayListRepository.findOne({ _id: req.params.id }).then(playlist => {
+                res.render('admin/playlist_edit', { playlist, musics })
+            })
         })
     }
-    updatePlaylist(req, res) {
-        PlayListRepository.findOneAndUpdate({ _id: req.params.id }, { name: req.body.name }).then(() => {
-            req.flash('notify', 'Playlist mise à jour !');
-            res.redirect('/admin/playlist_list')
-        })
+    async updatePlaylist(req, res) {
+        if(req.body.songs && req.body.songs.length > 0) {
+            let songs = [];
+            let song;
+            for(song of req.body.songs) {
+                await musicRepository.findOne({ _id: song }).then((music) => {       
+                    songs.push(music)         
+                });
+            }
+            PlayListRepository.findOneAndUpdate({ _id: req.params.id }, { $set: { songs } }).then(() => {
+                req.flash( 'notify', `La musique a bien été ajouté à la playlist !`);
+                //res.redirect('/admin/playlist')
+                res.redirect('/admin/playlist_list')
+            })
+        } else {
+            req.flash( 'error', `Une erreur inconnue est survenu !`);
+            //res.redirect('/admin/playlist')
+            res.redirect('/admin/playlist/edit/'+req.params.id)
+        }
     }
 
     newPlaylist(req, res) {
